86492efee9cc2cf7d5492c916e601834ab184566
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-11-24 15:18:16 +0700

    Fix wrong tag name if default_resource_naming is disabled

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   tags = {
-    Name          = "${module.repo_name.name}"
+    Name          = "${var.default_resource_naming == "true" ? module.repo_name.name : var.repo_name}"
     ProductDomain = "${var.product_domain}"
     Environment   = "${var.environment}"
     ManagedBy     = "terraform"
